3c9b48e077d858f502a2a60cd011ae081befe736
Naimat Ansari <123116024+NaimatAnsari@users.noreply.github.com>
2024-12-22 14:12:13 +0500

    add

---
+++ AccessModifires.php
@@ -0,0 +1,41 @@
+<?php
+
+class Teachers {
+    private function questions() {
+        echo 'Important questions';
+    }
+
+    public function seeTeachers() {
+        $this->questions();
+    }
+
+    protected function result() {
+        echo 'Result is Ready';
+    }
+
+}
+
+class Manager extends Teachers {
+    
+    public function result() {
+        parent::result();
+    }
+}
+
+
+$s1 = new Teachers();
+$s1 = $s1->seeTeachers();
+
+
+$s2 = new Manager();
+$s2 = $s2->result();
+
+
+
+
+
+
+
+
+
+?>
