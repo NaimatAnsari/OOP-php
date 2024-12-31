82e50a348a540aefbcce34e639109bd654028c29
Naimat Ansari <123116024+NaimatAnsari@users.noreply.github.com>
2024-12-31 15:46:26 +0500

    add

--- OOP-MySQL/ReadData.php
+++ OOP-MySQL/ReadData.php
@@ -8,46 +8,46 @@ class User {
         $this->db_conn = $conn;
     }
 
-    public function fetch_data(){
+    public function fetch_data() {
+        // Prepare and execute the query
         $getData = $this->db_conn->prepare("SELECT * FROM users");
         $getData->execute();
         $result = $getData->fetchAll();
-        foreach ($result as $user) {
-            
-            echo"
-             <table border='1px'>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Gender</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>". $user['id'] . "</td>
-                <td>". $user['first_name'] . "</td>
-                <td>". $user['last_name'] . "</td>
-                <td>". $user['email'] . "</td>
-                <td>". $user['gender'] . "</td>
-            </tr>
-        </tbody>
-    </table>
-            
-            
-            ";
-            
-        }
-    }
 
+        // Start the table
+        echo "
+        <table border='1px'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Gender</th>
+                </tr>
+            </thead>
+            <tbody>";
+
+        // Loop through the results and generate rows
+        foreach ($result as $user) {
+            echo "
+                <tr>
+                    <td>" . $user['id'] . "</td>
+                    <td>" . $user['first_name'] . "</td>
+                    <td>" . $user['last_name'] . "</td>
+                    <td>" . $user['email'] . "</td>
+                    <td>" . $user['gender'] . "</td>
+                </tr>";
+        }
+
+        // Close the table
+        echo "
+            </tbody>
+        </table>";
+    }
 }
 
+// Create a new User object and fetch data
 $user1 = new User($conn);
 $user1->fetch_data();
-
-
-
-
 ?>
