b228a61591f040686f1c86fa34cf64fb7ec0288c
Naimat Ansari <123116024+NaimatAnsari@users.noreply.github.com>
2025-01-02 23:13:13 +0500

    add

--- trait.php
+++ trait.php
@@ -12,16 +12,41 @@
         }
     }
 
+
+    trait branch3 {
+        public function branch3Name ($name) {
+            echo "This Branch name is $name";
+        }
+    }
+
+    trait branch4 {
+        public function branch4Name($name) {
+            echo "This Branch name is $name";
+        }
+    }
+
     class Company {
         use branch1;
         use branch2;
+        use branch3;
+        use branch4;
+
+        public function bankName($name) {
+            echo "<h1>This Bank name is $name</h1>";
+        }
     }
 
     $c1 = new Company();
+    $c1->bankName("Faysal Bank Pakistan");
+    echo "<hr>";
     $c1->branch1Name("Landhi Branch"); 
-    echo "<br>";
+    echo "<hr>";
     $c1->branch2Name("Orangi Town Branch");
-
+    echo "<hr>";
+    $c1->branch3Name("Malir Branch"); 
+    echo "<hr>";
+    $c1->branch4Name("Shahfaisal Branch");
+    
     
 
 ?>
