9b816a8377503447577edcfc5baa8f9507dd7251
Naimat Ansari <123116024+NaimatAnsari@users.noreply.github.com>
2025-01-02 23:45:34 +0500

    add

--- interface.php
+++ interface.php
@@ -5,7 +5,7 @@
         public function productName($name);
         public function productPrice($price);
         public function productBrand($brand);
-
+        public function productSellArea($area);
     }
 
     class ProductsFeatures implements Products {
@@ -22,8 +22,20 @@
             echo "The Product Brand Name is $brand. ";
         }
 
+        function productSellArea($area){
+            echo "The Product Sell Area is $area";
+        }
+
     }
 
+$product = new ProductsFeatures();
+$product->productName("Redmi A3");
+echo "<hr>";
+$product->productPrice(22,300);
+echo "<hr>";
+$product->productBrand("Xaomi");
+echo "<hr>";
+$product->productSellArea("Quaidabad Mobile Market");
     
 
 ?>
